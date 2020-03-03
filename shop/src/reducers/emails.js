6d2c4e2418db9203dea2ff2f57f4bd8286e4981a
eldom93 <eldomiatyn@yahoo.com>
2020-03-03 10:58:33 -0600

    add:toggleemailreducer

--- shop/src/reducers/emails.js
+++ shop/src/reducers/emails.js
@@ -10,6 +10,16 @@ const emails = (state = [], action) => {
                     }
                 ]
             })
+            case TOGGLE_EMAIL:
+            return Object.assign({}, state, {
+                emails: state.emails.map((email, index) => {
+                    if(index === action.index) {
+                        return Object.assign({}, todo, {selected: !email.selected
+                        })
+                    }
+                    return email
+                })
+            })
             default:
                 return state
     }

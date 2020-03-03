0d85bbe3256726c620f1d2d7807ce9de81142fcd
eldom93 <eldomiatyn@yahoo.com>
2020-03-03 11:02:04 -0600

    add:visibilityfilterreducer

--- shop/src/reducers/visibilityFilter.js
+++ shop/src/reducers/visibilityFilter.js
@@ -1,7 +1,12 @@
 import { VisibilityFilters } from '../actions'
 
-const visibilityFilters = (state = VisibilityFilters.Show_ALL, action) =>{
-
+const visibilityFilters = (state = VisibilityFilters.SHOW_ALL, action) =>{
+    switch (action.type) {
+        case SET_VISIBILITY_FILTER:
+            return action.filter
+        default: 
+            return state
+    }
 }
 
 export default visibilityFilters;
